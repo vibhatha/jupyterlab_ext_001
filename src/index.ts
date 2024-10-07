f3f2e5169c73b383e5769b1f03e22d97da8b6805
Vibhatha Lakmal Abeykoon <vibhatha@gmail.com>
2024-10-07 16:08:54 +0530

    fix: v1

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,130 @@ import { ICommandPalette, MainAreaWidget } from '@jupyterlab/apputils';
 
 import { Widget } from '@lumino/widgets';
 
+interface APODResponse {
+  copyright: string;
+  date: string;
+  explanation: string;
+  media_type: 'video' | 'image';
+  title: string;
+  url: string;
+};
+
+class APODWidget extends Widget {
+  /**
+  * Construct a new APOD widget.
+  */
+  constructor() {
+    super();
+
+    this.addClass('my-apodWidget');
+
+    // Add an image element to the panel
+    this.img = document.createElement('img');
+    this.node.appendChild(this.img);
+
+    // Add a summary element to the panel
+    this.summary = document.createElement('p');
+    this.node.appendChild(this.summary);
+  }
+
+  /**
+  * The image element associated with the widget.
+  */
+  readonly img: HTMLImageElement;
+
+  /**
+  * The summary text element associated with the widget.
+  */
+  readonly summary: HTMLParagraphElement;
+
+  /**
+  * Handle update requests for the widget.
+  */
+  async updateAPODImage(): Promise<void> {
+
+    const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${this.randomDate()}`);
+
+    if (!response.ok) {
+      const data = await response.json();
+      if (data.error) {
+        this.summary.innerText = data.error.message;
+      } else {
+        this.summary.innerText = response.statusText;
+      }
+      return;
+    }
+
+    const data = await response.json() as APODResponse;
+
+    if (data.media_type === 'image') {
+      // Populate the image
+      this.img.src = data.url;
+      this.img.title = data.title;
+      this.summary.innerText = data.title;
+      if (data.copyright) {
+        this.summary.innerText += ` (Copyright ${data.copyright})`;
+      }
+    } else {
+      this.summary.innerText = 'Random APOD fetched was not an image.';
+    }
+  }
+
+  /**
+  * Get a random date string in YYYY-MM-DD format.
+  */
+  randomDate(): string {
+    const start = new Date(2010, 1, 1);
+    const end = new Date();
+    const randomDate = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+    return randomDate.toISOString().slice(0, 10);
+  }
+}
+
+/**
+* Activate the APOD widget extension.
+*/
+function activate(app: JupyterFrontEnd, palette: ICommandPalette) {
+  console.log('JupyterLab extension jupyterlab_apod is activated!');
+
+  // Define a widget creator function
+  const newWidget = () => {
+    const content = new APODWidget();
+    const widget = new MainAreaWidget({ content });
+    widget.id = 'apod-jupyterlab';
+    widget.title.label = 'Astronomy Picture';
+    widget.title.closable = true;
+    return widget;
+  }
+
+  // Create a single widget
+  let widget = newWidget();
+
+  // Add an application command
+  const command: string = 'apod:open';
+  app.commands.addCommand(command, {
+    label: 'Random Astronomy Picture',
+    execute: () => {
+      // Regenerate the widget if disposed
+      if (widget.isDisposed) {
+        widget = newWidget();
+      }
+      if (!widget.isAttached) {
+        // Attach the widget to the main work area if it's not there
+        app.shell.add(widget, 'main');
+      }
+      // Refresh the picture in the widget
+      widget.content.updateAPODImage();
+      // Activate the widget
+      app.shell.activateById(widget.id);
+
+    }
+  });
+
+  // Add the command to the palette.
+  palette.addItem({ command, category: 'Tutorial' });
+}
+
 /**
  * Initialization data for the jupyterlab_ext_001 extension.
  */
@@ -15,45 +139,7 @@ const plugin: JupyterFrontEndPlugin<void> = {
   description: 'A JupyterLab extension.',
   autoStart: true,
   requires: [ICommandPalette],
-  activate: (app: JupyterFrontEnd, palette: ICommandPalette) => {
-    console.log('JupyterLab extension jupyterlab_ext_001 is activated!');
-    console.log('Working on a new feature!!!');
-    console.log('ICommandPalette:', palette);
-
-    // Define a widget creator function,
-    // then call it to make a new widget
-    const newWidget = () => {
-      // Create a blank content widget inside of a MainAreaWidget
-      const content = new Widget();
-      const widget = new MainAreaWidget({ content });
-      widget.id = 'apod-jupyterlab';
-      widget.title.label = 'Astronomy Picture';
-      widget.title.closable = true;
-      return widget;
-    }
-    let widget = newWidget();
-
-    // Add an application command
-    const command: string = 'apod:open';
-    app.commands.addCommand(command, {
-      label: 'Random Astronomy Picture',
-      execute: () => {
-        // Regenerate the widget if disposed
-        if (widget.isDisposed) {
-          widget = newWidget();
-        }
-        if (!widget.isAttached) {
-          // Attach the widget to the main work area if it's not there
-          app.shell.add(widget, 'main');
-        }
-        // Activate the widget
-        app.shell.activateById(widget.id);
-      }
-    });
-
-    // Add the command to the palette.
-    palette.addItem({ command, category: 'Tutorial' });
-  }
+  activate: activate
 };
 
 export default plugin;
